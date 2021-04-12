04f49735eb7a0e5d472875d0b4158c1f41643253
Lucy <lucy@poulton.xyz>
2021-04-12 18:25:40 +0100

    add kyori as a dependency, added display name method

--- watercore-base/src/main/java/me/lucyy/watercore/api/user/WaterCoreUser.java
+++ watercore-base/src/main/java/me/lucyy/watercore/api/user/WaterCoreUser.java
@@ -1,5 +1,6 @@
 package me.lucyy.watercore.api.user;
 
+import net.kyori.adventure.text.Component;
 import org.jetbrains.annotations.Nullable;
 import java.util.UUID;
 
@@ -22,6 +23,18 @@ public class WaterCoreUser {
 	 */
 	@Nullable
 	public static WaterCoreUser fromUuid(final UUID uuid) {
+		return null; // TODO
+	}
+
+	/**
+	 * Gets the user's display name. This will vary depending on the enabled modules, but may contain:
+	 * <ul>
+	 *     <li>nicknames</li>
+	 *     <li>prefixes and suffixes for ranks</li>
+	 * </ul>
+	 *
+	 */
+	public Component getDisplayName() {
 		return null; // TODO
 	}
 }

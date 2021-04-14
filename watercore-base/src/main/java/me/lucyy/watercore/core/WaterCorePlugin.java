ca76b1d26c9b9a2bc99cb45d0c4a1b69b71d4cdf
BuildTools <unconfigured@null.spigotmc.org>
2021-04-13 21:26:45 -0500

    Worked on MySQL and configuration (YAML) file management.
    Added new packages for both config and MySQL classes. Added static FileConfiguration variable config for the default config (inherited from JavaPlugin)
    
    Added JDBC drivers to pom.xml, in order to access their Connection class and better PreparedStatements.

--- watercore-base/src/main/java/me/lucyy/watercore/core/WaterCorePlugin.java
+++ watercore-base/src/main/java/me/lucyy/watercore/core/WaterCorePlugin.java
@@ -1,18 +1,25 @@
 package me.lucyy.watercore.core;
 
 import me.lucyy.watercore.api.impl.WaterCoreImpl;
+import me.lucyy.watercore.core.mysql.MySql;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class WaterCorePlugin extends JavaPlugin {
 	private WaterCoreImpl waterCore;
+	public static FileConfiguration config;
 
 	@Override
 	public void onEnable() {
+		saveDefaultConfig();
+		config = getConfig();
+
 		waterCore = new WaterCoreImpl();
 	}
 
 	@Override
 	public void onDisable() {
+		MySql.instance.close();
 		// Plugin shutdown logic
 	}
 }
